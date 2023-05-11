e1294c822c717873fe35f466c6551981f808ca72
Youssef Alsaeed <yousefalsaeed2002@gmail.com>
2023-05-11 22:39:02 +0300

    Structure edits

---
+++ Tools-Akeel/src/main/java/entity/Meal.java
@@ -0,0 +1,42 @@
+package entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Id;
+
+@Entity
+public class Meal {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int Id;
+	private String name;
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	private double price;
+
+	@ManyToOne
+	@JoinColumn(name="restaurantId")
+	private Restaurant restaurant;
+
+
+}
---
+++ Tools-Akeel/src/main/java/entity/Restaurant.java
@@ -0,0 +1,43 @@
+package entity;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Id;
+
+
+@Entity
+public class Restaurant {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int Id;
+	private String name;
+	private int ownerId;
+	@OneToMany
+	@JoinColumn(name="restaurantId")
+	private List<Meal>meals;
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getOwnerId() {
+		return ownerId;
+	}
+	public void setOwnerId(int ownerId) {
+		this.ownerId = ownerId;
+	}
+
+}
