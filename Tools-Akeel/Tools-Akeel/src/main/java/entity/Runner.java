d2834167fbf2a8c996854dce4400d3130343f72b
Youssef Alsaeed <yousefalsaeed2002@gmail.com>
2023-05-11 22:48:45 +0300

    Restructured project

---
+++ Tools-Akeel/Tools-Akeel/src/main/java/entity/Runner.java
@@ -0,0 +1,42 @@
+package entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Runner {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private String id;
+	private String name;
+	private boolean isAvailable;
+	private double deliveryFees;
+
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public boolean isAvailable() {
+		return isAvailable;
+	}
+	public void setAvailable(boolean isAvailable) {
+		this.isAvailable = isAvailable;
+	}
+	public double getDeliveryFees() {
+		return deliveryFees;
+	}
+	public void setDeliveryFees(double deliveryFees) {
+		this.deliveryFees = deliveryFees;
+	}
+
+}
