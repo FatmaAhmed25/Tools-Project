13d601410f13b4536484a1b14cd2d83f053cbceb
youssefessmat <109749787+youssefessmat@users.noreply.github.com>
2023-05-22 13:50:15 +0300

    added api method markOrderAsDelivered
    this method update the order status to delivered if its on delivering state ,updates also saved to RestaurantReport

--- Tools-Akeel/src/main/java/Controller/RunnerService.java
+++ Tools-Akeel/src/main/java/Controller/RunnerService.java
@@ -1,23 +1,60 @@
 package Controller;
 
-import javax.ejb.Stateless;
-import javax.persistence.*;
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
-import entity.*;
+import entity.Order;
+import entity.OrderStatus;
+import entity.Restaurant;
+import entity.RestaurantReport;
+import entity.Runner;
 
-@Path("/runner")
+import javax.annotation.security.RolesAllowed;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+import java.util.List;
+//@RolesAllowed("RUNNER")
 @Stateless
-@Consumes(MediaType.APPLICATION_JSON)
-@Produces(MediaType.APPLICATION_JSON)
+@Path("/runner")
 public class RunnerService {
+	@PersistenceContext
+	private EntityManager entityManager;
+	@Path("/markOrderAsDelivered/{orderId}/{runnerID}")
+	@PUT
+    public String markOrderAsDelivered(@PathParam("orderId")String orderId,@PathParam("runnerID") String runnerId) {
+        Order order =entityManager.find(Order.class,orderId);
+        Runner runner = entityManager.find(Runner.class,runnerId);
+
+        
+        if (order.getStatus().equals(OrderStatus.PREPARING)) {
+            return "Order is not accepted yet '~' ";
+        }
+        if (order.getStatus() == OrderStatus.CANCELED) {
+            return "the order is already canceled '~' ";
+        }
+        if (order.getStatus() == OrderStatus.DELIVERED) {
+            return "the order is already delivered '-' ";
+        }
+        
+        if (order.getStatus().equals(OrderStatus.DELIVERING)) {
+            order.setStatus(OrderStatus.DELIVERED);
+        
+            runner.setAvailable(true);
+            entityManager.persist(order);
+            entityManager.persist(runner);
+            Restaurant restaurant =order.getRestauarant();
+            RestaurantReport restaurantReport = restaurant.getRestaurantReport();
+            restaurantReport.addNumberOfCompletedOrders();
+            restaurantReport.addTotalAmountEarned(order.getTotal());
+
+        }
+        return "orderId: " + orderId + "  is deliverd ;-) ";
+    }
 	
-	UserService us;
-	Runner runner = us.getCurrentRunner();
-	
-	@GET
-	public void checkRunner()
-	{
-		System.out.println(runner.getName());
-	}
+
+
+
 }
